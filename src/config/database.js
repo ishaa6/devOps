086a58229ae9a8a445e7bfae605e8de8a3e8065e
Shreyaa <shreyaaanand6@gmail.com>
2026-01-18 18:29:10 +0530

    Dockerization

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,13 @@
 import 'dotenv/config';
 
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { neon, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+
+if (process.env.DATABASE_URL === 'development') {
+  neonConfig.fetchEndpoint = 'https://neon-local:5432/sql';
+  neonConfig.useSecureWebSocket = false;
+  neonConfig.poolQueryViaFetch = true; 
+}
 
 const sql = neon(process.env.DATABASE_URL);
 const db = drizzle(sql);
